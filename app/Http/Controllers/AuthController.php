a8bc71a99e41275a5ecdac5ee5211be35defe841
Arsema <arsema.solomonbarhane>
2026-09-23 12:27:51 +0200

    opdracht 5 is klaar en je kan nu uitloggen

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,6 +35,16 @@ class AuthController extends Controller
     public function login()
     {
 
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('show.login');
     }
 
 }
